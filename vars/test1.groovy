a1f2f8e95d16c3038577de3b2fd4bb1ae64dcd23
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-02-28 11:18:30 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,12 +1,8 @@
-node {
-    def mvnHome
-    stage('Preparation') { 
-        echo "hello from shared lib 1"
-    }
-    stage('Build') {
-        
-    }
-    stage('Results') {
-        
+def call(name){
+    node {
+        def mvnHome
+        stage('Preparation') { 
+            echo "${name} hello from shared lib 1"
+        }
     }
 }
